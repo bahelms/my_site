a7e54bdab92e54ab49cef682873f368e3fcacdd1
Jimbonk <jimbonkgit@gmail.com>
2013-08-19 07:25:57 -0400

    Starts my personal website/blog

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+ruby '2.0.0'
+
+gem 'rails', '4.0.0'
+gem 'pg', '0.16.0'
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'bootstrap-sass', '2.3.2.1'
+
+group :development, :test do
+  gem 'rspec-rails', '2.14.0'
+end
+
+group :test do
+  gem 'capybara', '2.1.0'
+end
+
+group :production do
+  gem 'rails_12factor', '0.0.2'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MySite::Application.config.secret_key_base = 'c2e0560a25390dc326639037855ebc4f6e14ea6f7d5ba48102a78c35af4540af7ce1083190fc18959ac7ec135138761871574d386ca5f7c7c50985336a532ad2'
